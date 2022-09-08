37d02e3feac60700db1f56b5028776279e0759bf
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-09-07 23:33:22 -0500

    Footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -40,7 +40,7 @@ export default function Navbar() {
                             {/* Profile dropdown */}
                             <Menu as="div" className="relative ml-12">
                                 <div>
-                                    <Menu.Button className="rounded-full p-1 mx-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                    <Menu.Button className="rounded-full p-1 mx-2 text-gray-400 hover:text-white">
                                         <span className="sr-only">Open user menu</span>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 lg:hidden">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Meridian from '../components/Meridian'
 import Carousel from '../components/Carousel'
 import UAHOVR from '../components/UAHOVR'
 import Extras from '../components/Extras'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Carousel />
       <UAHOVR />
       <Extras />
+      <Footer />
     </div>
   )
 }
