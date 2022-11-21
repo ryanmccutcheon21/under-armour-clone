14748eede57c32324ce62608c5c221b9239e2a6e
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-11-21 17:38:22 -0600

    Fix slice TypeError

--- pages/components/Products.js
+++ pages/components/Products.js
@@ -4,12 +4,11 @@ import { useState } from 'react'
 
 const Products = ({ products }) => {
     const [num, setNum] = useState(15)
-    const productsArr = [...products]
 
     return (
         <div className='flex flex-col ml-[30%]'>
             <ul className='flex flex-wrap my-5'>
-                {productsArr.slice(0, num).map(product => (
+                {products?.slice(0, num).map(product => (
                     <li key={product.image.url} className='m-2 border border-gray-500 rounded hover:cursor-pointer hover:shadow-xl shadow-gray-600 hover:scale-105'>
                         <Image alt='Product Image' width={product.image.width} height={product.image.height} src={product.image.url} />
                     </li>
